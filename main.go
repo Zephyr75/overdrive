c52958e7a12ae43ab0634301afd8c1c1b3c2710a
Zephyr75 <antoine.familleroger@gmail.com>
2023-07-16 00:58:38 +0200

    Progress on Mesh class

--- main.go
+++ main.go
@@ -49,25 +49,18 @@ func main() {
   gl.Init()
   gl.Enable(gl.DEPTH_TEST)
 
-  vertices := []float32{
-    // positions         // normal          // texture coords
-     0.5,  0.5,  0.5,    1.0,  1.0,  1.0,   1.0, 1.0,
-     0.5, -0.5,  0.5,    1.0, -1.0,  1.0,   1.0, 0.0,
-    -0.5, -0.5,  0.5,   -1.0, -1.0,  1.0,   0.0, 0.0,
-    -0.5,  0.5,  0.5,   -1.0,  1.0,  1.0,   0.0, 1.0,
-     0.5,  0.5, -0.5,    1.0,  1.0, -1.0,   0.0, 0.0,
-     0.5, -0.5, -0.5,    1.0, -1.0, -1.0,   0.0, 1.0,
-    -0.5, -0.5, -0.5,   -1.0, -1.0, -1.0,   1.0, 1.0,
-    -0.5,  0.5, -0.5,   -1.0,  1.0, -1.0,   1.0, 0.0,
-  }
-
-  // cubePositions := []mgl32.Vec3{
-  //   { 0.0,  0.0,  0.0}, 
-  //   { 2.0,  5.0, -15.0},
-  //   {-1.5,  5.0, -2.5},
-  //   {-3.8,  2.0, -12.3},
-  //   { 2.4,  0.4, -3.5},
+  // vertices := []float32{
+  //   // positions         // normal          // texture coords
+  //    0.5,  0.5,  0.5,    1.0,  1.0,  1.0,   1.0, 1.0,
+  //    0.5, -0.5,  0.5,    1.0, -1.0,  1.0,   1.0, 0.0,
+  //   -0.5, -0.5,  0.5,   -1.0, -1.0,  1.0,   0.0, 0.0,
+  //   -0.5,  0.5,  0.5,   -1.0,  1.0,  1.0,   0.0, 1.0,
+  //    0.5,  0.5, -0.5,    1.0,  1.0, -1.0,   0.0, 0.0,
+  //    0.5, -0.5, -0.5,    1.0, -1.0, -1.0,   0.0, 1.0,
+  //   -0.5, -0.5, -0.5,   -1.0, -1.0, -1.0,   1.0, 1.0,
+  //   -0.5,  0.5, -0.5,   -1.0,  1.0, -1.0,   1.0, 0.0,
   // }
+
 
   // lightPositions := []mgl32.Vec3{
   //   {-1.7,  3.0, -7.5},
@@ -77,21 +70,21 @@ func main() {
   //   {-1.3,  1.0, -1.5},
   // }
 
-  indices := []uint32{
-    0, 1, 3,
-    1, 2, 3,
-    0, 1, 4,
-    1, 4, 5,
-    0, 3, 4,
-    3, 4, 7,
-    1, 2, 5,
-    2, 5, 6,
-    2, 3, 6,
-    3, 6, 7,
-    4, 5, 6,
-    4, 6, 7,
+  // indices := []uint32{
+  //   0, 1, 3,
+  //   1, 2, 3,
+  //   0, 1, 4,
+  //   1, 4, 5,
+  //   0, 3, 4,
+  //   3, 4, 7,
+  //   1, 2, 5,
+  //   2, 5, 6,
+  //   2, 3, 6,
+  //   3, 6, 7,
+  //   4, 5, 6,
+  //   4, 6, 7,
 
-  }
+  // }
 
   // DECLARE SHADERS
   vertexShaderFile, err := os.ReadFile("shaders/cubes.vert.glsl")
@@ -110,72 +103,72 @@ func main() {
     panic(err)
   }
 
-  vertexShaderFile, err = os.ReadFile("shaders/light.vert.glsl")
-  if err != nil {
-    panic(err)
-  }
-  vertexShaderSource = string(vertexShaderFile) + "\x00"
+  // vertexShaderFile, err = os.ReadFile("shaders/light.vert.glsl")
+  // if err != nil {
+  //   panic(err)
+  // }
+  // vertexShaderSource = string(vertexShaderFile) + "\x00"
 
-  fragmentShaderFile, err = os.ReadFile("shaders/light.frag.glsl")
-  if err != nil {
-    panic(err)
-  }
-  fragmentShaderSource = string(fragmentShaderFile) + "\x00"
-  lightsProgram, err := opengl.CreateProgram(vertexShaderSource, fragmentShaderSource)
+  // fragmentShaderFile, err = os.ReadFile("shaders/light.frag.glsl")
+  // if err != nil {
+  //   panic(err)
+  // }
+  // fragmentShaderSource = string(fragmentShaderFile) + "\x00"
+  // lightsProgram, err := opengl.CreateProgram(vertexShaderSource, fragmentShaderSource)
 
 
 
-  // Declare VBO and EBO
-  var EBO uint32
-  gl.GenBuffers(1, &EBO)
-  var VBO uint32
-  gl.GenBuffers(1, &VBO)
+  // // Declare VBO and EBO
+  // var EBO uint32
+  // gl.GenBuffers(1, &EBO)
+  // var VBO uint32
+  // gl.GenBuffers(1, &VBO)
 
-  // Declare main VAO
-  var cubesVAO uint32
-  gl.GenVertexArrays(1, &cubesVAO)
+  // // Declare main VAO
+  // var cubesVAO uint32
+  // gl.GenVertexArrays(1, &cubesVAO)
 
-  // Bind VAO to VBO and gl.VertexAttribPointer, gl.EnableVertexAttribArray calls
-  gl.BindVertexArray(cubesVAO)
-  // Copy VBO to an OpenGL buffer
-  gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-  // Define OpenGL buffer structure
-  gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
-  // Copy EBO to an OpenGL buffer
-  gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
-  // Define OpenGL buffer structure
-  gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
+  // // Bind VAO to VBO and gl.VertexAttribPointer, gl.EnableVertexAttribArray calls
+  // gl.BindVertexArray(cubesVAO)
+  // // Copy VBO to an OpenGL buffer
+  // gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+  // // Define OpenGL buffer structure
+  // gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+  // // Copy EBO to an OpenGL buffer
+  // gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
+  // // Define OpenGL buffer structure
+  // gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
 
-  // Define Vertex Attrib to be used by the shader
-  gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
-  gl.EnableVertexAttribArray(0)
-  gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(3*4))
-  gl.EnableVertexAttribArray(1)
-  gl.VertexAttribPointer(2, 2, gl.FLOAT, false, 8*4, gl.PtrOffset(6*4))
-  gl.EnableVertexAttribArray(2)
-  
-  // Declare lights VAO
-  var lightsVAO uint32
-  gl.GenVertexArrays(1, &lightsVAO)
+  // // Define Vertex Attrib to be used by the shader
+  // gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
+  // gl.EnableVertexAttribArray(0)
+  // gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(3*4))
+  // gl.EnableVertexAttribArray(1)
+  // gl.VertexAttribPointer(2, 2, gl.FLOAT, false, 8*4, gl.PtrOffset(6*4))
+  // gl.EnableVertexAttribArray(2)
+  // 
+  // // Declare lights VAO
+  // var lightsVAO uint32
+  // gl.GenVertexArrays(1, &lightsVAO)
 
-  // Bind VAO to VBO and gl.VertexAttribPointer, gl.EnableVertexAttribArray calls
-  gl.BindVertexArray(lightsVAO)
-  // Copy VBO to an OpenGL buffer
-  gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-  // Copy EBO to an OpenGL buffer
-  gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
-  // Define Vertex Attrib to be used by the shader
-  gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
-  gl.EnableVertexAttribArray(0)
+  // // Bind VAO to VBO and gl.VertexAttribPointer, gl.EnableVertexAttribArray calls
+  // gl.BindVertexArray(lightsVAO)
+  // // Copy VBO to an OpenGL buffer
+  // gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+  // // Copy EBO to an OpenGL buffer
+  // gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
+  // // Define Vertex Attrib to be used by the shader
+  // gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
+  // gl.EnableVertexAttribArray(0)
 
   
   // Reset OpenGL buffers
-  gl.BindBuffer(gl.ARRAY_BUFFER, 0)
-  gl.BindVertexArray(0)
+  // gl.BindBuffer(gl.ARRAY_BUFFER, 0)
+  // gl.BindVertexArray(0)
 
 
   // DECLARE TEXTURES
-  texture, err := opengl.CreateTexture("textures/square.png")
+  // texture, err := opengl.CreateTexture("textures/square.png")
 
  
   // FULL WINDOW LIFECYCLE
@@ -187,12 +180,12 @@ func main() {
     gl.ClearColor(0.2, 0.3, 0.3, 1.0)
     gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     
-    gl.BindTexture(gl.TEXTURE_2D, texture)
+    // gl.BindTexture(gl.TEXTURE_2D, texture)
 
     
     gl.UseProgram(cubesProgram)
    
-    gl.BindVertexArray(cubesVAO)
+    // gl.BindVertexArray(cubesVAO)
 
     currentFrame := glfw.GetTime()
     deltaTime = float32(currentFrame - lastFrame)
@@ -213,42 +206,48 @@ func main() {
     modelLoc := gl.GetUniformLocation(cubesProgram, gl.Str("model\x00"))
     gl.UniformMatrix4fv(modelLoc, 1, false, &model[0])
 
-    lightColorLoc := gl.GetUniformLocation(cubesProgram, gl.Str("lightColor\x00"))
-    gl.Uniform3f(lightColorLoc, 1.0, 0.0, 1.0)
+    // lightColorLoc := gl.GetUniformLocation(cubesProgram, gl.Str("lightColor\x00"))
+    // gl.Uniform3f(lightColorLoc, 1.0, 0.0, 1.0)
 
-    lightPosLoc := gl.GetUniformLocation(cubesProgram, gl.Str("lightPos\x00"))
-    gl.Uniform3f(lightPosLoc, 1.2, float32(glfw.GetTime()) - 5.0, 1.0)
+    // lightPosLoc := gl.GetUniformLocation(cubesProgram, gl.Str("lightPos\x00"))
+    // gl.Uniform3f(lightPosLoc, 1.2, float32(glfw.GetTime()) - 5.0, 1.0)
 
-    viewPosLoc := gl.GetUniformLocation(cubesProgram, gl.Str("viewPos\x00"))
-    gl.Uniform3f(viewPosLoc, scene.Cam.Pos.X(), scene.Cam.Pos.Y(), scene.Cam.Pos.Z())
+    // viewPosLoc := gl.GetUniformLocation(cubesProgram, gl.Str("viewPos\x00"))
+    // gl.Uniform3f(viewPosLoc, scene.Cam.Pos.X(), scene.Cam.Pos.Y(), scene.Cam.Pos.Z())
 
-    gl.DrawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, gl.PtrOffset(0))
+    // gl.DrawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+
+
+
+
+
 
     // Draw lights
 
-    gl.UseProgram(lightsProgram)
+    // gl.UseProgram(lightsProgram)
 
-    gl.BindVertexArray(lightsVAO)
-
-
-    view = mgl32.LookAtV(scene.Cam.Pos, scene.Cam.Pos.Add(scene.Cam.Front), scene.Cam.Up)
-    viewLoc = gl.GetUniformLocation(lightsProgram, gl.Str("view\x00"))
-    gl.UniformMatrix4fv(viewLoc, 1, false, &view[0])
+    // gl.BindVertexArray(lightsVAO)
 
 
-    projection = mgl32.Perspective(mgl32.DegToRad(scene.Cam.Fov), float32(settings.WindowWidth)/float32(settings.WindowHeight), 0.1, 100.0)
-    projectionLoc = gl.GetUniformLocation(lightsProgram, gl.Str("projection\x00"))
-    gl.UniformMatrix4fv(projectionLoc, 1, false, &projection[0])
+    // view = mgl32.LookAtV(scene.Cam.Pos, scene.Cam.Pos.Add(scene.Cam.Front), scene.Cam.Up)
+    // viewLoc = gl.GetUniformLocation(lightsProgram, gl.Str("view\x00"))
+    // gl.UniformMatrix4fv(viewLoc, 1, false, &view[0])
 
-    model = mgl32.Translate3D(1.2, float32(glfw.GetTime()) - 5.0, 1.0)
-    modelLoc = gl.GetUniformLocation(lightsProgram, gl.Str("model\x00"))
-    gl.UniformMatrix4fv(modelLoc, 1, false, &model[0])
+
+    // projection = mgl32.Perspective(mgl32.DegToRad(scene.Cam.Fov), float32(settings.WindowWidth)/float32(settings.WindowHeight), 0.1, 100.0)
+    // projectionLoc = gl.GetUniformLocation(lightsProgram, gl.Str("projection\x00"))
+    // gl.UniformMatrix4fv(projectionLoc, 1, false, &projection[0])
+
+    // model = mgl32.Translate3D(1.2, float32(glfw.GetTime()) - 5.0, 1.0)
+    // modelLoc = gl.GetUniformLocation(lightsProgram, gl.Str("model\x00"))
+    // gl.UniformMatrix4fv(modelLoc, 1, false, &model[0])
+
+    // gl.DrawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+
 
     // gl.DrawArrays(gl.TRIANGLES, 0, 36)
-    gl.DrawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, gl.PtrOffset(0))
-
-
-
     window.SwapBuffers()
     glfw.PollEvents()
   }
--- scene/mesh.go
+++ scene/mesh.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/mathgl/mgl32"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+  "overdrive/opengl"
 )
 
 type MeshXml struct {
@@ -15,14 +20,20 @@ type MeshXml struct {
 }
 
 type Mesh struct {
+  Positions []mgl32.Vec3
+  NormalCoords []mgl32.Vec3
+  TextureCoords []mgl32.Vec2
+
   Vertices [][]float32
   Faces [][]uint32
+
+  vbo uint32
+  vao uint32
+  ebo uint32
 }
 
 func (m MeshXml) ToMesh() Mesh {
-  var mesh Mesh
-  mesh = mesh.loadData(m.Obj, m.Mtl)
-  return mesh
+  return mesh.loadData(m.Obj, m.Mtl)
 }
 
 
@@ -34,12 +45,16 @@ func (m Mesh) loadData(obj, mtl string) Mesh {
   }
   defer objFile.Close()
 
-  var positions []float32
-  var normals []float32
-  var textures []float32
-  var vertices []float32
+  // var positions []float32
+  // var normals []float32
+  // var textures []float32
+  // var vertices []float32
   var faces [][]uint32
   var face []uint32
+
+  var positions []mgl32.Vec3
+  var normalCoords []mgl32.Vec3
+  var textureCoords []mgl32.Vec2
 
   objScanner := bufio.NewScanner(objFile) 
   for objScanner.Scan() {
@@ -56,17 +71,20 @@ func (m Mesh) loadData(obj, mtl string) Mesh {
       switch line[1] {
       case ' ': 
         third, _ := strconv.ParseFloat(split_line[2], 32)
-        positions = append(positions, float32(first))
-        positions = append(positions, float32(second))
-        positions = append(positions, float32(third))
+        positions = append(positions, mgl32.Vec3{float32(first), float32(second), float32(third)})
+        // positions = append(positions, float32(first))
+        // positions = append(positions, float32(second))
+        // positions = append(positions, float32(third))
       case 't':
-        textures = append(textures, float32(first))
-        textures = append(textures, float32(second))
+        textureCoords = append(textureCoords, mgl32.Vec2{float32(first), float32(second)})
+        // textures = append(textures, float32(first))
+        // textures = append(textures, float32(second))
       case 'n':
         third, _ := strconv.ParseFloat(split_line[2], 32)
-        normals = append(normals, float32(first))
-        normals = append(normals, float32(second))
-        normals = append(normals, float32(third))
+        normalCoords = append(normalCoords, mgl32.Vec3{float32(first), float32(second), float32(third)})
+        // normals = append(normals, float32(first))
+        // normals = append(normals, float32(second))
+        // normals = append(normals, float32(third))
       }
     case 'u':
       faces = append(faces, face)
@@ -85,24 +103,105 @@ func (m Mesh) loadData(obj, mtl string) Mesh {
   }
   faces = append(faces, face)
 
-  for i := 0; i < len(faces); i++ {
-    for j := 0; j < len(faces[i]); j+=3 {
-      posIndex := faces[i][j] - 1
-      texIndex := faces[i][j+1] - 1
-      normIndex := faces[i][j+2] - 1
-      vertices = append(vertices, positions[posIndex*3])
-      vertices = append(vertices, positions[posIndex*3+1])
-      vertices = append(vertices, positions[posIndex*3+2])
-      vertices = append(vertices, normals[normIndex*3])
-      vertices = append(vertices, normals[normIndex*3+1])
-      vertices = append(vertices, normals[normIndex*3+2])
-      vertices = append(vertices, textures[texIndex*2])
-      vertices = append(vertices, textures[texIndex*2+1])
-    }
-  }
+  // for i := 0; i < len(faces); i++ {
+  //   for j := 0; j < len(faces[i]); j+=3 {
+  //     posIndex := faces[i][j] - 1
+  //     texIndex := faces[i][j+1] - 1
+  //     normIndex := faces[i][j+2] - 1
+  //     vertices = append(vertices, positions[posIndex*3])
+  //     vertices = append(vertices, positions[posIndex*3+1])
+  //     vertices = append(vertices, positions[posIndex*3+2])
+  //     vertices = append(vertices, normals[normIndex*3])
+  //     vertices = append(vertices, normals[normIndex*3+1])
+  //     vertices = append(vertices, normals[normIndex*3+2])
+  //     vertices = append(vertices, textures[texIndex*2])
+  //     vertices = append(vertices, textures[texIndex*2+1])
+  //   }
+  // }
 
-  m.Vertices = append(m.Vertices, vertices)
+  // m.Vertices = append(m.Vertices, vertices)
   m.Faces = faces
+  m.Positions = positions
+  m.NormalCoords = normalCoords
+  m.TextureCoords = textureCoords
+
+  m.fillVertices()
 
   return m
+}
+
+func (m *Mesh) fillVertices() {
+  for i := 0; i < len(m.Faces); i++ {
+    for j := 0; j < len(m.Faces[i]); j+=3 {
+      posIndex := m.Faces[i][j] - 1
+      texIndex := m.Faces[i][j+1] - 1
+      normIndex := m.Faces[i][j+2] - 1
+      m.Vertices = append(m.Vertices, []float32{
+        m.Positions[posIndex][0], 
+        m.Positions[posIndex][1], 
+        m.Positions[posIndex][2], 
+        m.NormalCoords[normIndex][0], 
+        m.NormalCoords[normIndex][1], 
+        m.NormalCoords[normIndex][2], 
+        m.TextureCoords[texIndex][0], 
+        m.TextureCoords[texIndex][1],
+      })
+    }
+  }
+}
+
+
+func (m *Mesh) Setup() {
+  // Declare VBO, EBO and VAO
+  gl.GenBuffers(1, &m.ebo)
+  gl.GenBuffers(1, &m.vbo)
+  gl.GenVertexArrays(1, &m.vao)
+
+  // Bind VAO to VBO and gl.VertexAttribPointer, gl.EnableVertexAttribArray calls
+  gl.BindVertexArray(m.vao)
+  // Copy VBO to an OpenGL buffer
+  gl.BindBuffer(gl.ARRAY_BUFFER, m.vbo)
+  // Define OpenGL buffer structure
+  gl.BufferData(gl.ARRAY_BUFFER, len(m.Vertices)*4, gl.Ptr(m.Vertices), gl.STATIC_DRAW)
+  // Copy EBO to an OpenGL buffer
+  gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, m.ebo)
+  // Define OpenGL buffer structure
+  gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(m.Faces)*4, gl.Ptr(m.Faces), gl.STATIC_DRAW)
+
+  // Define Vertex Attrib to be used by the shader
+  gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
+  gl.EnableVertexAttribArray(0)
+  gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(3*4))
+  gl.EnableVertexAttribArray(1)
+  gl.VertexAttribPointer(2, 2, gl.FLOAT, false, 8*4, gl.PtrOffset(6*4))
+  gl.EnableVertexAttribArray(2)
+
+  // Clear VAO binding
+  gl.BindVertexArray(0)
+}
+
+func (m *Mesh) Draw(program uint32, scene *Scene) {
+
+   lightColorLoc := gl.GetUniformLocation(program, gl.Str("lightColor\x00"))
+   gl.Uniform3f(lightColorLoc, 1.0, 0.0, 1.0)
+
+   lightPosLoc := gl.GetUniformLocation(program, gl.Str("lightPos\x00"))
+   gl.Uniform3f(lightPosLoc, 1.2, float32(glfw.GetTime()) - 5.0, 1.0)
+
+   viewPosLoc := gl.GetUniformLocation(program, gl.Str("viewPos\x00"))
+   gl.Uniform3f(viewPosLoc, scene.Cam.Pos.X(), scene.Cam.Pos.Y(), scene.Cam.Pos.Z())
+
+
+  // assign specular, diffuse and whatever
+
+  texture, err := opengl.CreateTexture("textures/square.png")
+  if err != nil {
+    panic(err)
+  }
+  gl.BindTexture(gl.TEXTURE_2D, texture)
+
+  gl.BindVertexArray(m.vao)
+  gl.DrawElements(gl.TRIANGLES, int32(len(m.Faces)), gl.UNSIGNED_INT, gl.PtrOffset(0))
+  gl.BindVertexArray(0)
+
 }
