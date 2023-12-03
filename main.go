d4478f9c7e9b233d551d6173302929c2f4ea2255
Zephyr75 <antoine.familleroger@gmail.com>
2023-12-03 20:39:34 +0100

    Moved ecs test to main

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
   "overdrive/core"
+  "overdrive/ecs"
 	"github.com/Zephyr75/gutter/ui"
   "image/color"
+  "time"
 )
 
 
 /////////////
+
+type HealthBar struct {
+	health int
+}
+func (HealthBar) Component() string { return "HealthBar" }
+
+type Name struct {
+	firstName string
+}
+func (Name) Component() string { return "Name" }
+
 
 
 func main() {
@@ -19,6 +32,52 @@ func main() {
 	}
 
 	app.Run(MainWindow)
+
+  bob := ecs.Entity{Name{"Bob"}, HealthBar{100}}
+  dylan := ecs.Entity{Name{"Dylan"}}
+	world := ecs.World{}
+
+	// Systems
+	renameSystem := ecs.NewSystem(
+    &world, 
+    func(entity ecs.Entity) ecs.Entity {
+      name := entity.Get("Name").(Name)
+      println(name.firstName)
+      name.firstName = "Bobby"
+      entity.Set("Name", name)
+      return entity
+    },
+    &bob,
+    &dylan,
+  )
+
+  loseHealthSystem := ecs.NewSystem(
+    &world,
+    func(entity ecs.Entity) ecs.Entity {
+      healthBar := entity.Get("HealthBar").(HealthBar)
+      println(healthBar.health)
+      healthBar.health -= 1
+      entity.Set("HealthBar", healthBar)
+      return entity
+    },
+    &bob,
+  )
+
+	// World
+	world.AddEntities(&bob)
+  world.AddEntities(&dylan)
+	world.AddUpdateSystems(loseHealthSystem)
+
+  // println(bob.healthBar.health)
+  renameSystem.RunOnEntities([]*ecs.Entity{&bob, &dylan})
+	renameSystem.RunOnQuery([]string{"Name", "HealthBar"})
+  renameSystem.RunOnTargets()
+
+  world.Init()
+  world.Update(time.Second / 60)
+
+  time.Sleep(2 * time.Second)
+
 
 }
 
